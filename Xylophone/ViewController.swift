1089c59b90793bbcbeaefe308325eda5a9cfcfdc
Flamez - Oluwole Adebiyi <flamekeed@gmail.com>
2019-11-28 00:20:44 +0100

    Completed Project

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,35 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound(soundName: sender.currentTitle!)
+    }
     
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+            player = try! AVAudioPlayer(contentsOf: url!)
+            player.play()
+            
+        } catch let error {
 
+            print(error.localizedDescription)
+        }
+                
+    }
 }
+
 
